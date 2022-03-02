f5727c08346d5dd3e18f8938588da9ff2c95d587
Emawyer <evan.mawyer@yahoo.com>
2022-03-02 18:03:38 -0500

    update

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -10,9 +10,10 @@ resource "aws_db_instance" "database" {
   engine_version         = "8.0"
   instance_class         = "db.t2.micro"
   identifier             = "${var.namespace}-db-instance"
-  name                   = "pets"
-  username               = "admin"
-  password               = random_password.password.result
+  name                   = "evoluit"
+  username               = "luit"
+  access key               = aws_access_key_id
+  secret access key      = aws_secret_access_key_id
   db_subnet_group_name   = var.vpc.database_subnet_group #B
   vpc_security_group_ids = [var.sg.db] #B
   skip_final_snapshot    = true
